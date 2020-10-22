a43ad56cc2f51c15292f483114aa0dabbd62083d
Sam Evans <evansst@icloud.com>
2020-10-21 19:56:00 -0600

    Done.

--- array_manipulation.js
+++ array_manipulation.js
@@ -1,30 +1,51 @@
-function arrayManipulation(n, queries) {
-  let array = Array(n).fill(0)
+// export function arrayManipulation(n, queries) {
+//   let array = Array(n).fill(0)
+//   let max = -Infinity
 
-  for(let i = 0; i < queries.length; i++) {
-    // array = array.map((e, j) => {
-    //   return (j >= queries[i][0] - 1 && j <= queries[i][1] - 1) 
-    //     ? e + queries[i][2]
-    //     : e
-    // })
-    for(let j = queries[i][0] - 1; j <= queries[i][1] - 1; j++) {
-      array[j] += queries[i][2]
-    }
+//   for(let i = 0; i < queries.length; i++) {
+//     for(let j = queries[i][0] - 1; j < queries[i][1]; j++) {
+//       array[j] += queries[i][2]
+//       max = array[j] > max ? array[j] : max
+//     }
+//   }
+
+//   return max
+// }
+
+// export const arrayManipulation = (n, queries) => {
+//   let array = [
+//     [1, n, 0]
+//   ]
+
+//   for(const query of queries) {
+//     for(let i = 0; i < array.length; i++) {
+//       if(array[i][0] > query[1] || array[i][1] < query[0]) {
+//         array.push(query)
+//         break
+//       } else if(array[i][0] > query[0] && array[i][1] < query[1]) {
+//         array[i] = query
+//         break
+//       } else {
+//         console.log(array[i], query)
+//       }
+//     }
+//   }
+//   return array
+// }
+
+export const arrayManipulation = (n, queries) => {
+  let array = Array(n + 1).fill(0)
+  let max = 0
+
+  for(const [a, b, k] of queries) {
+    array[a] += k
+    array[b + 1] -= k
   }
 
-  // return array.reduce((max, e) => e > max ? e : max)
-  return Math.max(...array)
+  for(let i = 1; i < array.length; i++) {
+    array[i] += array[i - 1]
+    max = array[i] > max ? array[i] : max
+  }
+  
+  return max
 }
-
-console.log( 
-  arrayManipulation(
-    10,
-    [
-      [1, 5, 3],
-      [4, 8, 7],
-      [6, 9, 1],
-    ]
-  )
-)
-
-[ 1, 9, 16, 16, 31, 24, 16, 16, 15, 0 ]
